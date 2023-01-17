5b57f718cbd65490b591f05428ff73960d2b603e
Allie Zhao <allie.k.zhao@gmail.com>
2023-01-17 12:38:01 -0800

    setting up Main component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
+import Main from './components/Main/Main';
 
 function App() {
   return (
     <section className="main">
       <Header />
-      {/* add your main component here */}
+      <Main />
       <Footer />
     </section>
   );
---
+++ src/components/Main/Main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Main.css';
+
+export default function Main({ post }) {
+  return <div className="honk">blog posts will go here</div>;
+}
